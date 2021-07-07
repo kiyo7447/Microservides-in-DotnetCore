2ae9c6264a239a9e11a73d227aafe5946aa24039
Kiyotaka Abe <kiyo7447@gmaill.com>
2021-07-07 23:08:18 +0900

    update simple rest api

--- Chapter02/ShoppingCart/ShoppingCart/ShoppingCartController.cs
+++ Chapter02/ShoppingCart/ShoppingCart/ShoppingCartController.cs
@@ -1,48 +1,70 @@
-namespace ShoppingCart.Shoppingcart
+﻿namespace ShoppingCart.Shoppingcart
 {
-  using System.Threading.Tasks;
-  using EventFeed;
-  using Microsoft.AspNetCore.Mvc;
-  using ShoppingCart;
+	using System.Diagnostics;
+	using System.Text.Json;
+	using System.Threading.Tasks;
+	using EventFeed;
+	using Microsoft.AspNetCore.Mvc;
+	using ShoppingCart;
 
-  [Route("/shoppingcart")]
-  public class ShoppingCartController : ControllerBase
-  {
-    private readonly IShoppingCartStore shoppingCartStore;
-    private readonly IProductCatalogClient productCatalog;
-    private readonly IEventStore eventStore;
+	[Route("/shoppingcart")]
+	public class ShoppingCartController : ControllerBase
+	{
+		private readonly IShoppingCartStore shoppingCartStore;
+		private readonly IProductCatalogClient productCatalog;
+		private readonly IEventStore eventStore;
 
-    public ShoppingCartController(
-      IShoppingCartStore shoppingCartStore,
-      IProductCatalogClient productCatalog,
-      IEventStore eventStore)
-    {
-      this.shoppingCartStore = shoppingCartStore;
-      this.productCatalog = productCatalog;
-      this.eventStore = eventStore;
-    }
+		public ShoppingCartController(
+		  IShoppingCartStore shoppingCartStore,
+		  IProductCatalogClient productCatalog,
+		  IEventStore eventStore)
+		{
+			this.shoppingCartStore = shoppingCartStore;
+			this.productCatalog = productCatalog;
+			this.eventStore = eventStore;
+		}
 
-    [HttpGet("{userId:int}")]
-    public ShoppingCart Get(int userId) => this.shoppingCartStore.Get(userId);
+		[HttpGet("{userId:int}")]
+		public ShoppingCart Get(int userId) => this.shoppingCartStore.Get(userId);
 
-    [HttpPost("{userId:int}/items")]
-    public async Task<ShoppingCart> Post(int userId, [FromBody] int[] productIds)
-    {
-      var shoppingCart = shoppingCartStore.Get(userId);
-      var shoppingCartItems = await this.productCatalog.GetShoppingCartItems(productIds);
-      shoppingCart.AddItems(shoppingCartItems, eventStore);
-      this.shoppingCartStore.Save(shoppingCart);
+		[HttpPost("{userId:int}/items")]
+		public async Task<ShoppingCart> Post(int userId, [FromBody] int[] productIds)
+		{
+			var shoppingCart = shoppingCartStore.Get(userId);
+			var shoppingCartItems = await this.productCatalog.GetShoppingCartItems(productIds);
+			shoppingCart.AddItems(shoppingCartItems, eventStore);
+			this.shoppingCartStore.Save(shoppingCart);
 
-      return shoppingCart;
-    }
+			return shoppingCart;
+		}
 
-    [HttpDelete("{userid:int}/items")]
-    public ShoppingCart Delete(int userId, [FromBody] int[] productIds)
-    {
-      var shoppingCart = this.shoppingCartStore.Get(userId);
-      shoppingCart.RemoveItems(productIds, this.eventStore);
-      this.shoppingCartStore.Save(shoppingCart);
-      return shoppingCart;
-    }
-  }
+		[HttpDelete("{userid:int}/items")]
+		public ShoppingCart Delete(int userId, [FromBody] int[] productIds)
+		{
+			var shoppingCart = this.shoppingCartStore.Get(userId);
+			shoppingCart.RemoveItems(productIds, this.eventStore);
+			this.shoppingCartStore.Save(shoppingCart);
+			return shoppingCart;
+		}
+
+		//応答データの書式設定
+		//https://docs.microsoft.com/ja-jp/aspnet/core/web-api/advanced/formatting?view=aspnetcore-5.0
+
+		[HttpPost("hoge/{str}")]
+		public Hoge GetHoge(string str, [FromBody] Rootobject hoge)
+		{
+			Debug.WriteLine($"str={str}");
+			Debug.WriteLine($"hoge={hoge}");
+			return new Hoge { I = 123, N = "abc" };
+		}
+		public record Rootobject
+		{
+			public Hoge? Root { get; set; }
+		}
+		public record Hoge
+		{
+			public int I { get; set; }
+			public string N { get; set; } = "";
+		}
+	}
 }
